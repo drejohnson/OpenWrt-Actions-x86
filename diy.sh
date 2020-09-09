d1c022390ed040f98b9b69ed5e86a793eafc3fdb
Dre Johnson <dre@phreshr.com>
2020-09-10 01:39:50 +0800

    update

--- diy.sh
+++ diy.sh
@@ -1,6 +1,1 @@
 #!/bin/sh
-
-cp -rf default-settings package/default-settings
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone https://github.com/OpenWrt-Actions/smartdns.git package/smartdns
-git clone https://github.com/OpenWrt-Actions/luci-app-smartdns-compat.git package/luci-app-smartdns-compat
