bf4cc76a903c2f05ee2471cff27263925ea491a3
Dre Johnson <dre@phreshr.com>
2020-09-27 08:52:38 +0800

    clean up

--- passwall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "src-git helloworld https://github.com/OpenWrt-Actions/helloworld" >> feeds.conf.default
-echo "src-git lienol https://github.com/OpenWrt-Actions/openwrt-package-lienol" >> feeds.conf.default
-./scripts/feeds clean
-./scripts/feeds update -a
