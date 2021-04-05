5b3192a48edfacc177aff67bfe33ca3ac9e186be
Dre Johnson <dre@phreshr.com>
2021-04-05 11:59:10 +0800

    update build

--- diy.sh
+++ diy.sh
@@ -11,3 +11,10 @@ svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/l
 # 编译 po2lmo (如果有po2lmo可跳过)
 cd package/luci-app-openclash/tools/po2lmo
 make && sudo make install
+
+echo '
+CONFIG_CRYPTO_CHACHA20_X86_64=y
+CONFIG_CRYPTO_POLY1305_X86_64=y
+' >> ./target/linux/x86/64/config-5.4
+
+sed -i "s/enabled '0'/enabled '1'/g" feeds/packages/utils/irqbalance/files/irqbalance.config
